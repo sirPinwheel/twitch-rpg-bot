4dec3a6b59d97fa0efafa43ff8c85b10f0f1af6c
sirPinwheel <pinwheelgm+github@gmail.com>
2019-07-28 14:52:13 +0200

    General update

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-HOST = 'irc.twitch.tv'
-PORT = 6667
-NAME = ''
-OAUTH = '' # 'oauth:sdfgsdfsdf'
-CHANNEL = '' # '#gsdgsgd'
+HOST = 'irc.twitch.tv' # default is irc.twitch.tv
+PORT = 6697 # default with SSL support is 6697
+NAME = '' # 'Nickofthebot'
+OAUTH = '' # 'oauth:token'
+CHANNEL = '' # '#channelname'
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 from irc_client import IrcClient
 from conf import *
 
