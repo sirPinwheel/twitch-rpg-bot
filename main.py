9538d139df91dd4fa6a0b42162a0288af7826170
nizkiren <emb@niz-kiren.de>
2019-07-28 13:03:24 +0200

    Add !help command (very basic)

--- main.py
+++ main.py
@@ -28,10 +28,30 @@ def mirror_msg(msg):
     if msg.split()[0] == "mirror":
         send(msg[::-1])
 
+def help_msg(msg):
+    # split message by whitespaes
+    parts = msg.split()
+    # check if it is a PRIVMS (chat message) and if it starts with !help
+    if parts[1] != "PRIVMSG" or parts[3] != ":!help":
+        return
+    # remove first entries, so that we only have to worry about things after !help
+    parts = parts[4:]
+    
+    # parse sub category for !help
+    if len(parts) == 0:
+        send("Available help commands are: char and game")
+    elif parts[0] == "char":
+        send("Help for character creation and development")
+    elif parts[0] == "game":
+        send("Help for game mechanics")
+    else:
+        send("There is not help page for %s!" % (parts[0]))
+
 # HANDLERS variable has to be below handler functions
 HANDLERS = [
 	print_msg,
-    mirror_msg
+    mirror_msg,
+    help_msg
 ]
 
 # Registering handlers to be used when processing messages
@@ -48,3 +68,5 @@ while True:
         irc_client.disconnect()
         del irc_client
         break
+    elif command:
+        send(command)
